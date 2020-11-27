b9ab5a6df3ad27043c804eff75511849d71771dd
husseinajam <hussein_ali_ejam@yahoo.com>
2020-11-27 17:44:21 +0300

    update clipper.dart

--- lib/widget/clipper.dart
+++ lib/widget/clipper.dart
@@ -8,14 +8,21 @@ import 'package:flutter/material.dart';
 class OvalRightBorderClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    var path = Path();
+/*    var path = Path();
     path.lineTo(0, 0);
     path.lineTo(size.width-40, 0);
     path.quadraticBezierTo(
         size.width, size.height / 4, size.width, size.height/2);
     path.quadraticBezierTo(
         size.width, size.height - (size.height / 4), size.width-40, size.height);
+    path.lineTo(0, size.height);*/
+
+    var path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
+
     return path;
   }
 
